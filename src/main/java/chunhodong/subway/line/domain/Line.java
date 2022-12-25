53fedccd33701f94991c023403f3a078d91873db
chunhodong <yhms4432@naver.com>
2022-12-25 23:54:27 +0900

    feat: 노선도메인 이름수정 기능 추가

--- src/main/java/chunhodong/subway/line/domain/Line.java
+++ src/main/java/chunhodong/subway/line/domain/Line.java
@@ -2,12 +2,14 @@ package chunhodong.subway.line.domain;
 
 import chunhodong.subway.line.exception.LineException;
 import chunhodong.subway.line.exception.LineExceptionCode;
+import lombok.Getter;
 import org.apache.logging.log4j.util.Strings;
 
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Getter
 public class Line {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,23 +18,24 @@ public class Line {
     private String name;
     @Enumerated(EnumType.STRING)
     private LineColor color;
-
     public Line(String name, LineColor color) {
-        validateLine(name,color);
+        validateLine(name, color);
         this.name = name;
         this.color = color;
     }
-
     public Line() {
 
     }
-
-    private void validateLine(String name,LineColor color) {
+    private void validateLine(String name, LineColor color) {
         if (Strings.isEmpty(name)) {
             throw new LineException(LineExceptionCode.EMPTY_NAME);
         }
-        if(Objects.isNull(color)){
+        if (Objects.isNull(color)) {
             throw new LineException(LineExceptionCode.EMPTY_COLOR);
         }
+    }
+    public void modifyName(String name){
+        validateLine(name,color);
+        this.name = name;
     }
 }
--- src/test/java/chunhodong/subway/line/domain/LineTest.java
+++ src/test/java/chunhodong/subway/line/domain/LineTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Line도메인테스트")
@@ -12,7 +13,7 @@ public class LineTest {
 
     @Nested
     @DisplayName("생성자는")
-    class DescribeCreateStation {
+    class DescribeCreateLine {
 
         @Nested
         @DisplayName("노선의 이름이 없으면 예외발생")
@@ -34,6 +35,40 @@ public class LineTest {
                         .isInstanceOf(LineException.class)
                         .hasMessageContaining("노선색상을 입력해야 합니다");
             }
+        }
+
+        @DisplayName("노선의 이름과 컬러를 입력하면 생성에 성공")
+        @Test
+        void returnLine() {
+            assertThat(new Line("2호선",LineColor.BLUE)).isNotNull();
+        }
+    }
+
+    @Nested
+    @DisplayName("이름 수정기능은 ")
+    class DescribeModifyLine {
+
+        @Nested
+        @DisplayName("노선의 이름이 없으면 예외발생")
+        class ContextWithNoName {
+            @Test
+            void throwsExeption() {
+                Line line = new Line("2호선",LineColor.BLUE);
+
+                assertThatThrownBy(() -> line.modifyName(null))
+                        .isInstanceOf(LineException.class)
+                        .hasMessageContaining("노선이름을 입력해야 합니다");
+            }
+        }
+
+        @DisplayName("노선의 이름을 입력하면 이름수정")
+        @Test
+        void returnLine() {
+            Line line = new Line("2호선",LineColor.BLUE);
+
+            line.modifyName("3호선");
+
+            assertThat(line.getName()).isEqualTo("3호선");
         }
     }
 }
