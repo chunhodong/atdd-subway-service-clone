f759a812de3a96277b7ef2d118e093701d3c179c
chunhodong <yhms4432@naver.com>
2022-12-30 15:32:49 +0900

    fix: StationResponse 생성자lombok 추가

--- src/main/java/chunhodong/subway/station/dto/StationResponse.java
+++ src/main/java/chunhodong/subway/station/dto/StationResponse.java
@@ -1,9 +1,13 @@
 package chunhodong.subway.station.dto;
 
 import chunhodong.subway.station.domain.Station;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StationResponse {
     private Long id;
     private String name;
