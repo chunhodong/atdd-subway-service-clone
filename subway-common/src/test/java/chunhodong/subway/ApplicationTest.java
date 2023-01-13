426667d5e2473070da93d7181d594f341d5b48a5
chunhodong <yhms4432@naver.com>
2023-01-13 23:38:30 +0900

    feat: common모듈에 테스트환경변수 파일추가

---
+++ subway-common/src/test/java/chunhodong/subway/ApplicationTest.java
@@ -0,0 +1,9 @@
+package chunhodong.subway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ApplicationTest {
+}
